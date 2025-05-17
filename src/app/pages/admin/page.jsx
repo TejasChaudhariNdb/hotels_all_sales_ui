36c36d023fd08f1f69e0cd620d5c1e0e8504c155
tejas <tejas.chaudhari@sitelucent.com>
2025-05-17 18:33:15 +0530

    Initial commit from Create Next App

---
+++ src/app/pages/admin/page.jsx
@@ -0,0 +1,5 @@
+// src/app/admin/page.tsx
+export default function AdminPage() {
+    return <h1>Admin Dashboard</h1>;
+  }
+  
---
+++ src/app/pages/user/page.jsx
@@ -0,0 +1,4 @@
+  export default function UserPage() {
+    return <h1>User Dashboard</h1>;
+  }
+  
